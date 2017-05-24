20f502e107f17962d8e53e6778b58cc485afb952
151070063 <151070063@smail.nju.edu.cn>
2017-05-24 16:29:01 +0800

    整合Protostuff和HttpClient

--- common-util/src/main/java/cn/sinjinsong/common/util/DateTimeUtil.java
+++ common-util/src/main/java/cn/sinjinsong/common/util/DateTimeUtil.java
@@ -1,63 +1,35 @@
 package cn.sinjinsong.common.util;
 
 import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by SinjinSong on 2017/4/23.
  */
 public class DateTimeUtil {
-    private static Map<String, Integer> months = new HashMap<>();
-    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    static {
-        months.put("January", 1);
-        months.put("February", 2);
-        months.put("March", 3);
-        months.put("April", 4);
-        months.put("May", 5);
-        months.put("June", 6);
-        months.put("July", 7);
-        months.put("August", 8);
-        months.put("September", 9);
-        months.put("October", 10);
-        months.put("November", 11);
-        months.put("December", 12);
-    }
-    
+    private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     public static LocalDateTime toLocalDateTime(Long date) {
         return LocalDateTime.ofInstant(Instant.ofEpochMilli(date), ZoneId.systemDefault());
     }
-
+    
     public static LocalDate toLocalDate(Long date) {
         return Instant.ofEpochMilli(date).atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
-    public static LocalDateTime toLocalDateTime(String date) {
-        StringBuilder sb = new StringBuilder();
-        String[] slices = date.split(" ");
-        sb.append(slices[2]);
-        sb.append("-");
-        sb.append(months.get(slices[1]));
-        sb.append("-");
-        sb.append(slices[0]);
-        try {
-            return toLocalDateTime(sdf.parse(sb.toString()).getTime());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    
-    public static long toLong(LocalDateTime localDateTime){
+    public static long toLong(LocalDateTime localDateTime) {
         return Timestamp.valueOf(localDateTime).getTime();
     }
-
+    
+    public static String formatLocalDateTime(LocalDateTime dateTime){
+        return dateTime.format(dateTimeFormatter);
+    }
+    
+    public static String formatLocalDateTime(Long dateTime){
+        return toLocalDateTime(dateTime).format(dateTimeFormatter);
+    }
+    
 }
--- common-util/src/main/java/cn/sinjinsong/common/util/FileUtil.java
+++ common-util/src/main/java/cn/sinjinsong/common/util/FileUtil.java
@@ -17,6 +17,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
 
 public final class FileUtil {
@@ -72,6 +76,13 @@ public final class FileUtil {
             throw new FileDownloadException(file.getName());
         }
     }
-
+    
+    public static void saveFile(String path, byte[] buf) throws IOException {
+        FileChannel outChannel = FileChannel.open(Paths.get(path),
+                StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+        MappedByteBuffer outMappedBuf = outChannel.map(FileChannel.MapMode.READ_WRITE, 0, buf.length);
+        outMappedBuf.put(buf);
+        outChannel.close();
+    }
 
 }
